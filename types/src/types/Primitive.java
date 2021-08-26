0bf5bbcefc30329817605419cda071a01517a9da
Gabriel Trindade <trindadi13@gmail.com>
2021-08-26 08:18:33 -0300

    estudos de tipos e hello world java

---
+++ types/src/types/Primitive.java
@@ -0,0 +1,22 @@
+package types;
+
+public class Primitive {
+
+	public static void main(String[] args) {
+		boolean bol = true; 
+		System.out.println("boolean, por default false" +" " + bol);
+		byte a = -127;
+		System.out.println("tipo numerico inteiro, 8bits de memoria com range de -127 até 127");
+		char b = '\u0000';
+		System.out.println("caracteres(um unico) da tabela ASCII(65k+ de caracteres), 16 bits");
+		short c = 32767; 
+		System.out.println("tipo numerico inteiro, 16 bits de memoria com range de 32,768  até 32,767");
+		int d = 2147483647;
+		System.out.println("tipo numerico inteiro, 32bits de memoria com range de -2,147,483,648 até 2,147,483,647");
+		float e = 0.3f;
+		System.out.println("tipo numerico flutuante, 32bits de memoria até 7 digitos decimais ");
+		double f = 0.3;
+		System.out.println("tipo numerico flutuante, 64bits de memoria até 16 digitos decimais");
+	}
+
+}
