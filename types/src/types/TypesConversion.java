0a4eafefd837813be71abe0d7e36c9f233c87d7f
Gabriel Trindade <trindadi13@gmail.com>
2021-08-26 08:36:12 -0300

    conversão de tipos de dados em java

---
+++ types/src/types/TypesConversion.java
@@ -0,0 +1,20 @@
+package types;
+
+public class TypesConversion {
+
+	public static void main(String[] args)
+	{
+		int inteiro = 10;
+		long inteiroLong = inteiro;
+		float floatLong = inteiroLong;
+		System.out.println(inteiro);
+		System.out.println(inteiroLong);
+		System.out.println(floatLong);
+		// conversão explicita
+		floatLong = 33.03f;
+		System.out.println(floatLong);
+		int inteiroFloat = (int)floatLong;
+		System.out.println(inteiroFloat); // ocorre perda de dados, int não aceita casas decimais
+	}
+
+}
