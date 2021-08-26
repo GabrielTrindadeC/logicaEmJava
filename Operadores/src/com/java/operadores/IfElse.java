78f79670bfbe63cb0388d58c8bfd69d4d9240331
Gabriel Trindade <trindadi13@gmail.com>
2021-08-26 15:21:22 -0300

    if e else

---
+++ Operadores/src/com/java/operadores/IfElse.java
@@ -0,0 +1,14 @@
+package com.java.operadores;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		int idade = 16;
+		if( idade >= 18) {
+			System.out.println("Adulto");
+		}else {
+			System.out.println("Menor");
+		}
+	}
+
+}
