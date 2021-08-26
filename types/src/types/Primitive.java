f6959a4a6f134fb6df49ea907d1e5e2a23ba5f19
Gabriel Trindade <trindadi13@gmail.com>
2021-08-26 08:26:18 -0300

    tinha esqueido do tipo long

--- types/src/types/Primitive.java
+++ types/src/types/Primitive.java
@@ -13,6 +13,8 @@ public class Primitive {
 		System.out.println("tipo numerico inteiro, 16 bits de memoria com range de 32,768  até 32,767");
 		int d = 2147483647;
 		System.out.println("tipo numerico inteiro, 32bits de memoria com range de -2,147,483,648 até 2,147,483,647");
+		long esqueciDessaAqui = 10000000000l;
+		System.out.println("tipo numerico inteiro, 64bits de memoria com range de -9,223,372,036,854,775,808(-2^63) até 9,223,372,036,854,775,807 (inclusive)(2^63 -1)");
 		float e = 0.3f;
 		System.out.println("tipo numerico flutuante, 32bits de memoria até 7 digitos decimais ");
 		double f = 0.3;
